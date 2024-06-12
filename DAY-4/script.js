74e43f78eb372c8941913d7f94a71024c6749977
Rohit Kumar <krohitgupta1502@gamil.com>
2024-06-12 09:10:09 +0530

    Day2 and Day4

---
+++ DAY-4/script.js
@@ -0,0 +1,41 @@
+console.log("Js working");
+var age; // declaration
+age = 15; // assignment | intialization
+console.log(age);
+
+age = 30; // reassignment
+console.log(age);
+
+var age = 20; // redeclaration
+console.log(age);
+
+function anc(){
+    var username = 3433;
+    console.log(age);
+}
+anc();
+
+//console.log(username); // function scope wala value use nahi kar sakte hai
+
+// In 'let' we can not decalration 
+let abc = "dhdjd";
+console.log(abc);
+let abc = "hdjdkd";
+
+// we can not use in out of the block mean {
+    
+// }
+{
+    let some = 20;
+}
+console.log(some);
+
+// We can not access the before 
+
+// eg 
+console.log(usename);
+let usename = "djkdld";
+console.log(usename);
+
+
+
