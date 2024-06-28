63585508e7d4bd7d5c2fdf51e174cf538ccf831b
Rohit Kumar <krohitgupta1502@gmail.com>
2024-06-28 13:30:12 +0530

    delete and patch

--- Day-15/controllers/productControllers.js
+++ Day-15/controllers/productControllers.js
@@ -1,7 +1,35 @@
 // const { productCollection } = require("../config/db")
 const productModel = require("../models/productModels");
 
+const checkId = async(req,res,next)=>{
+    try{
 
+        const {id} = req.params
+        const product = await productModel.findById(id);
+        if(!product){
+            res.status(404).json({
+                status:"fail",
+                message: "product ID not found"
+            })
+            return;
+           
+        }
+        next();
+    }
+    catch(err){
+        if(err.name == "CastError"){
+            res.status(400).json({
+                status: "Fail",
+                message:"Invailed product id"
+            })
+        }
+        res.status(500).json({
+            status: "fail",
+            err: "Internal Server Error"
+        })
+    }
+   
+}
 const getProducts = async (req,res)=>{
     
     // const products = await productCollection.find().toArray();
@@ -65,13 +93,66 @@ const replaceProduct = async (req, res)=>{
         console.log(err)
         console.log("Internal error in repalace product")
         console.log(Error.message)
-        info: err
+        res.status(500).json({
+            succuss: false,
+            message: "Internal server error",
+            info: err,
+        })
     }
 }
 
+const updateProduct = async (req,res)=>{
+  try{
+    const {id} = req.params
+    const body = req.body
+    body.updatedAt = Date.now();
+    const updatedproducts = await productModel.findOneAndUpdate({_id:id},body, {new:true})
+    res.status(200).json({
+        status: true,
+        data: updatedproducts
+    })
 
+}
+  catch(err){
+    console.log(err)
+    console.log("Internal error in updating product")
+    console.log(Error.message)
+    res.status(500).json({
+        succuss: false,
+        message: "Internal server error",
+        info: err,
+    })
+
+  }
+}
+const deleteProducts = async (req,res)=>{
+  try{
+    const {id} = req.params
+    await productModel.findOneAndDelete({_id:id})
+    res.status(204).json({
+        status: true,
+        data: null
+    })
+
+}
+  catch(err){
+    console.log(err)
+    console.log("Internal error in updating product")
+    console.log(Error.message)
+    res.status(500).json({
+        succuss: false,
+        message: "Internal server error",
+        info: err,
+    })
+
+  }
+}
 module.exports = {
     getProducts,
     createProduct,
-    replaceProduct
+    replaceProduct,
+    checkId,
+    updateProduct,
+    deleteProducts
+    
 }
--- Day-15/routes/productsRoutes.js
+++ Day-15/routes/productsRoutes.js
@@ -1,10 +1,19 @@
 const express = require("express");
-const { getProducts, createProduct, replaceProduct } = require("../controllers/productControllers");
+const {
+  getProducts,
+  createProduct,
+  replaceProduct,
+  checkId,
+  updateProduct,
+  deleteProducts,
+} = require("../controllers/productControllers");
 const productRouter = express.Router();
 
-productRouter.route("/")
-.get(getProducts)
-.post(createProduct)
-productRouter.route("/:id").put(replaceProduct)
+productRouter.route("/").get(getProducts).post(createProduct);
+productRouter
+  .route("/:id")
+  .put(checkId, replaceProduct)
+  .patch(checkId, updateProduct)
+  .delete(checkId, deleteProducts);
 
 module.exports = productRouter;
