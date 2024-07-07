dcf41549ad5e67b031c3bbf946363dd09596d7d1
Rohit Kumar <krohitgupta1502@gmail.com>
2024-07-07 08:13:47 +0530

    added product info card

--- Day-19-20-21/src/pages/homePage.js
+++ Day-19-20-21/src/pages/homePage.js
@@ -3,19 +3,18 @@ import ProductInfoCard from "../components/productinfoCard";
 import NavBar from "../components/navbar";
 import CatBar from "../components/categorybar";
 
-
 const HomePage = (props) => {
-  const {Catoge, productInfoCard, searchText, setSearchText} = props
+  const { Catoge, productInfoCard, setSearchText } = props;
   const navigate = useNavigate();
 
-  const openSearchPage = ()=> {
-    navigate("/search")
-  }
+  const openSearchPage = () => {
+    navigate("/search");
+  };
   return (
     <div>
-      <NavBar setSearchText={setSearchText} openSearchPage={openSearchPage}/>
-      <CatBar Catoge= {Catoge} />
-     
+      <NavBar setSearchText={setSearchText} openSearchPage={openSearchPage} />
+      <CatBar Catoge={Catoge} />
+
       <div className="home-card-page">
         <img
           src="https://images-eu.ssl-images-amazon.com/images/G/31/OHL_HSS/4.UNREC-PC-_hero_3000x1200-1unrec._CB569789166_.jpg"
@@ -23,7 +22,7 @@ const HomePage = (props) => {
         />
         <div className="home-card-page-container">
           {productInfoCard.map((elem) => {
-            return <ProductInfoCard data = {elem}/>;
+            return <ProductInfoCard data={elem} />;
           })}
         </div>
       </div>
